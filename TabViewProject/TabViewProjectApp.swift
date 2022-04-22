eb6df4da8e0f90690c21f899010c340fe9c8cfd0
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-22 17:27:10 +0200

    Initial Commit

---
+++ TabViewProject/TabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProjectApp.swift
+//  TabViewProject
+//
+//  Created by Esteban Calvete Iglesias on 22/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
