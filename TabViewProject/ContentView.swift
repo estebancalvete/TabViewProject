1b3638dcf3aad262e0bfe43f41aa544efc5f9c6d
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-22 17:41:58 +0200

    First commit

--- TabViewProject/ContentView.swift
+++ TabViewProject/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Home View")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            Text("Profile View")
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle.fill")
+                }
+        }
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
